783c6f637fc6fc3dd955e932af9569bb90b52e99
Samarth Patil <102871567+samarthpatilML@users.noreply.github.com>
2024-11-27 11:07:35 +0530

    update

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -16,9 +16,28 @@ const Projects = () => {
                                 <a href={project.link}
                                     target="_blank"
                                     rel="noopener noreferrer">
-                                        <img src={project.imgSrc} />
+                                    <img src={project.imgSrc} alt={project.title}
+                                    className="h-60 w-full object-cover"/>
                                 </a>
-
+                                <div className="p-6">
+                                    <h3 className="mb-2 text-lg fount-medium lg:text-2xl">{project.title} </h3>
+                                    <p className="mb-4">{project.description}</p>
+                                    <div className="mb-4">
+                                        <strong>Tech Stack:</strong>
+                                        <ul>
+                                            {project.techStack.map((tech, index) =>
+                                            (
+                                                <li key={index} className="mb-1
+                                                mr-2 inline-block rounded-full
+                                                border-2 broder-pink-500/30 px-3py-1 text-sm font-semibold">
+                                                    {tech}
+                                                </li>
+                                                
+                                              
+                                            ))}
+                                        </ul>
+                                    </div>
+                                </div>
                             </div>
                         </div>
 
